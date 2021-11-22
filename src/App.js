bc542ca0614cf46dca787752c2002b1a184c6a45
Gus Bikos <gusbikos@gmail.com>
2021-11-22 17:11:03 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1> Hey lets build a Twitter clone!</h1>
+
+      { /* Sidebar */ }
+
+      { /* Feed */ }
+
+      { /* Widgets */ }
+
+    </div>
+  )
+}
+
+export default App
